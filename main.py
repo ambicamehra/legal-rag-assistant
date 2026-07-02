dc0d50a0e07261d63ff3154e313310825d221b69
Ambica Mehra <ambicamehra@Ambicas-MacBook-Air.local>
2026-07-02 15:02:33 +0530

    Update legal corpus information

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.pipeline import legal_pipeline
 def main():
     llm=LLMmodel()
     st.title("Hi! I am a Legal Assistant")
-    st.caption("My knowledge base currently includes:\n1. The Consitution of India\n2. Securities and Exchange Board of India\n3. The Companies Act, 2013\n4. The Competition Act, 2002\n5. The Indian Contract Act, 1872\n6. The Insolvency and Bankruptcy Code (IBC), 2026")
+    st.caption("The system currently supports question answering across the following Indian legal documents:\n1. The Constitution of India\n2. SEBI Regulations, 2015\n3. The Companies Act, 2013\n4. The Competition Act, 2002\n5. The Indian Contract Act, 1872\n6. The Insolvency and Bankruptcy Code (IBC), 2016")
     query=st.chat_input("Ask away!")
     if query:
         with st.chat_message("user"):
